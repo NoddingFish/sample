42eae2cbcff67787ad83fa4ccde416d678a40d9a
徐然 <nlxuran@163.com>
2018-11-21 04:01:10 +0000

    Finish user destroy actions

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -13,4 +13,17 @@ class UserPolicy
     {
         return $currentUser->id === $user->id;
     }
+
+    /**
+     * 用户删除 只能是is_admin是管理员权限可以，并且不能删除自己
+     * @param User $currentUser
+     * @param User $user
+     * @return bool
+     * @author : XuRan
+     * @Time : 2018/11/21 0021 上午 11:48
+     */
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
+    }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'xuran';
         $user->email = 'nlxuran@163.com';
         $user->password = bcrypt('123456');
+        $user->is_admin = true;
         $user->save();
     }
 }
